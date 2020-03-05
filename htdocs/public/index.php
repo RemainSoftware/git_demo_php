0dacb79adf1be9ea27fcbc8930c871025d98c759
Vangelis Gkinis <vangelis.gkinis@remainsoftware.com>
2020-03-05 17:16:58 +0100

    GITDM-T0007: Git Demo
    Added some comments.

--- htdocs/public/index.php
+++ htdocs/public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+// I added a comment.
+
 // Define path to application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
